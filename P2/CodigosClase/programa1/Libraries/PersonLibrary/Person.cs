e04038b77684a06212a3eb58dca3c860e611afe0
Francisco Jiménez <106284960+Francesco-177@users.noreply.github.com>
2023-09-18 09:50:38 -0600

    second commit

---
+++ P2/CodigosClase/programa1/Libraries/PersonLibrary/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonLibrary;
+
+public class Person
+{
+    /*
+    Constructor: it's called when 
+    new() is executed instance 
+    Member: data inside class
+    public int number = 0;
+    public int getNumber() {return this,number }
+    public void setNumber (int:number){
+        this.number = number;
+    }
+
+    Propety: 
+    string Name {get; set;} //  simple propety
+
+
+    Two types:
+    Simple propety
+    Full Propety
+
+    Index : []
+    Operators: + - * / => &
+
+
+
+
+    */
+
+    
+
+}
